30b2f0e3cce3afac1635d7e9e597449a3710b15f
naveed <nav0285@gmail.com>
2015-03-14 03:59:45 -0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldTime::Application.config.secret_key_base = '8d761a7392d8cd7b15cfdf83064c1413501c775cb3fa3e30a69e84d2892d14f26fcc3fb49e7df32dc1f2f15b2d8257ce2c1f385a29c743beeef1a1db6dbf76b4'
